e0d62e5d8599d41ff510d5b1122a6677aa8a7523
Massimiliano Stucchi <mstucchi@ripe.net>
2017-05-03 20:24:04 +0200

    Add all the steps to the run script.

--- run.sh
+++ run.sh
@@ -15,3 +15,11 @@ wget "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.domain.gz"
 ./parse_domain_splitfile.py > domains.csv
 
 rm delegated-ripencc-extended-latest ripe.db.domain.gz
+
+./perPrefixStats.py
+
+./perPrefixStats_revDNSconsistency.py
+
+./computeCorrelations.py
+
+# tail -n+2 revDel_perPrefix_stats.csv | awk 'BEGIN{FS="|"; twentyfive = 0; fifty = 0; seventyfive = 0; hundred = 0;} {if ($5 <= 25) {twentyfive++;} else if (($5 > 25) && ($5 <= 50)) {fifty++;} else if (($5 > 50) && ($5 <= 75)) {seventyfive++;} else if (($5 > 75)) {hundred++;}} END{print(twentyfive " - " fifty " - " seventyfive " - " hundred);}'

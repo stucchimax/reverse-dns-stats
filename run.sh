7e5a5611a2e3f851c4aa0372f64994008c8c14ef
Massimiliano Stucchi <mstucchi@ripe.net>
2017-12-09 10:35:56 +0100

    Let's generate stats in a specific directory

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+DATE=`date "+%Y%m%d"`;
+
+mkdir $DATE
+cd $DATE
+
 # fetch delegated file
 # fetch split domain file
 
@@ -8,16 +13,16 @@ wget -q "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.domain.gz"
 
 # Get stats from delegated file
 
-./parse_delegated.py > delegated_parsed.csv
+../parse_delegated.py > delegated_parsed.csv
 
 # Get all the infos from the domain files
 
-./parse_domain_splitfile.py > domains.csv
+../parse_domain_splitfile.py > domains.csv
 
 rm delegated-ripencc-extended-latest ripe.db.domain.gz
 
-./perPrefixStats.py
+../perPrefixStats.py
 
-./computeCorrelations.py
+../computeCorrelations.py
 
 # tail -n+2 revDel_perPrefix_stats.csv | awk 'BEGIN{FS="|"; twentyfive = 0; fifty = 0; seventyfive = 0; hundred = 0;} {if ($5 <= 25) {twentyfive++;} else if (($5 > 25) && ($5 <= 50)) {fifty++;} else if (($5 > 50) && ($5 <= 75)) {seventyfive++;} else if (($5 > 75)) {hundred++;}} END{print(twentyfive " - " fifty " - " seventyfive " - " hundred);}'
